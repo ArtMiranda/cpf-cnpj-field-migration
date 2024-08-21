c28bdcd2c4b1032ff7a6c9881e824e1d40d843ac
Artur Miranda <artcamir12@gmail.com>
2024-08-21 11:11:10 -0300

    Initial commit

---
+++ src/main/java/br/dev/artumiranda/migrator/MigratorApplication.java
@@ -0,0 +1,13 @@
+package br.dev.artumiranda.migrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigratorApplication.class, args);
+	}
+
+}
